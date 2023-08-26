8824f1938190ded78032b9e4f8da1ab61443b215
rezasalehiii <rezasalehiii96@gmail.com>
2023-08-26 15:31:54 +0330

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -37,4 +37,8 @@ class MyApp extends StatelessWidget {
 }
 
 
-//jkgjyfyfyfytfytfhchgcycydcytfytv
+//git init
+// git remote add origin https://github.com/xxx/yyy.git
+// git add .
+// git commit -m "first commit"
+// git push -u origin master
